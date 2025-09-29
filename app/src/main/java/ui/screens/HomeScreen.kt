8f3edde723e755c6bf41f09cc6fafed4e14f1c02
aa-amp <aa.ampuero@duocuc.cl>
2025-09-29 19:33:11 -0300

    Adaptable a todos los tamaños de pantalla
    Adaptable a todos los tamaños de pantalla

--- app/src/main/java/ui/screens/HomeScreen.kt
+++ app/src/main/java/ui/screens/HomeScreen.kt
@@ -1,4 +1,4 @@
-package ui
+package ui.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
@@ -7,21 +7,22 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.appaaron_ampuero.R
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen() {
     Scaffold(
         topBar = {
-            TopAppBar(
-                title = { Text("AppAaron_Ampuero", color = MaterialTheme.colorScheme.secondary)
-                 }
+            TopAppBar( title = { Text("AppAaron_Ampuero", color = MaterialTheme.colorScheme.secondary)
+            }
             )
         }
     ) { innerPadding ->
@@ -33,14 +34,20 @@ fun HomeScreen() {
             verticalArrangement = Arrangement.spacedBy(20.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Bienvenido",
+            Text(
+                text = "Bienvenido",
                 style = MaterialTheme.typography.headlineMedium,
                 color = MaterialTheme.colorScheme.secondary
             )
-            Button(onClick = {/*accion futura*/ },
-                colors = ButtonDefaults.buttonColors(containerColor =
-                    MaterialTheme.colorScheme.secondary)) {
-                Text("Presioname",
+            Button(
+                onClick = {/*accion futura*/ },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor =
+                        MaterialTheme.colorScheme.secondary
+                )
+            ) {
+                Text(
+                    "Presioname",
                     color = MaterialTheme.colorScheme.onPrimary
                 )
             }
@@ -52,12 +59,20 @@ fun HomeScreen() {
                     .height(150.dp),
                 contentScale = ContentScale.Fit
             )
+            Divider(color = MaterialTheme.colorScheme.onSurface, thickness = 1.dp)
+            var text by remember { mutableStateOf("") }
+            TextField(
+                value = text,
+                onValueChange = { text = it },
+                label = { Text("Texto de relleno") },
+                modifier = Modifier.fillMaxWidth(),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = MaterialTheme.colorScheme.primary,
+                    unfocusedContainerColor = MaterialTheme.colorScheme.primaryContainer,
+                    focusedTextColor = MaterialTheme.colorScheme.surface,
+                    unfocusedTextColor = MaterialTheme.colorScheme.surface
+                )
+            )
         }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun HomeScreenPreview(){
-    HomeScreen()
 }
