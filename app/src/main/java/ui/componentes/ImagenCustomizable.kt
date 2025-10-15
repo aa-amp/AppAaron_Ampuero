a9d21fe0539d9db7733cd6a81d0bc037b93fab79
aa-amp <aa.ampuero@duocuc.cl>
2025-10-14 22:56:58 -0300

    lanzo desde un pixel 9a
    lanzo desde un pixel 9a

--- app/src/main/java/ui/componentes/ImagenCustomizable.kt
+++ app/src/main/java/ui/componentes/ImagenCustomizable.kt
@@ -11,37 +11,27 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
-import coil.request.ImageRequest
 import com.example.appaaron_ampuero.R
-
 
 @Composable
 fun ImagenCostumizable(uri: Uri?) {
     val context = LocalContext.current
 
-    if (uri != null) {
-        Image(
-            painter = rememberAsyncImagePainter(
-                ImageRequest.Builder(context)
-                    .data(uri)
-                    .crossfade(true)
-                    .build()
-            ),
-            contentDescription = "Foto de perfil",
-            modifier = Modifier
-                .size(120.dp)
-                .clip(CircleShape)
-        )
-    } else {
-        Image(
-            painter = painterResource(id = R.drawable.dueno),
-            contentDescription = "Foto por defecto",
-            modifier = Modifier
-                .size(120.dp)
-                .clip(CircleShape)
-        )
-    }
+    val painter = rememberAsyncImagePainter(
+        model = uri ?: R.drawable.dueno,
+        error = painterResource(id = R.drawable.dueno),
+        placeholder = painterResource(id = R.drawable.dueno)
+    )
+
+    Image(
+        painter = painter,
+        contentDescription = "Foto de perfil",
+        modifier = Modifier
+            .size(120.dp)
+            .clip(CircleShape)
+    )
 }
+
 
 
 
